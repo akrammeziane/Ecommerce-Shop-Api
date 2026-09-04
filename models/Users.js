0e738e3a33f120697132aebf2e41c510c0ce07c8
Akram Meziane <akram.meziane@univ-bouira.dz>
2026-09-04 23:28:10 +0100

    removing adress and phone from ValidRegister

--- models/Users.js
+++ models/Users.js
@@ -87,8 +87,6 @@ const ValidLogin = (user) => {
 const ValidRegister = (user) => {
   const registerValidationSchema = joi.object({
     name: joi.string().min(2).max(100).trim().required(),
-    phone: joi.string().min(10).max(15).trim(),
-    address: joi.string().min(5).max(200).trim(),
     email: joi.string().email().trim().lowercase().required(),
     password: passwordComplexity().required(),
   });
