7439bec74488c9530f5880578010945e9c1670c9
Akram Meziane <akram.meziane@univ-bouira.dz>
2026-09-14 02:51:13 +0100

    adding the filter to count InStock and Out Of Stock products

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -70,8 +70,8 @@ const getAllProducts = asyncHandler(async (req, res) => {
     await Promise.all([
       Product.find(filter).skip(skip).limit(pageSize),
       Product.countDocuments(filter),
-      Product.countDocuments({ quantity: { $gt: 0 } }),
-      Product.countDocuments({ quantity: { $eq: 0 } }),
+      Product.countDocuments({ ...filter, quantity: { $gt: 0 } }),
+      Product.countDocuments({ ...filter, quantity: { $eq: 0 } }),
     ]);
   const totalPages = Math.ceil(totalProducts / pageSize);
 
