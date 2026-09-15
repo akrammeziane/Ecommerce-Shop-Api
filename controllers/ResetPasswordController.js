39f88755d8d35e41cb07623bac9ba2a6c6bc5221
Akram Meziane <akram.meziane@univ-bouira.dz>
2026-09-15 02:43:02 +0100

    fix sendResetPasswordEmail by adding port and host to transporter

--- controllers/ResetPasswordController.js
+++ controllers/ResetPasswordController.js
@@ -30,7 +30,9 @@ const sendResetPasswordEmail = asyncHandler(async (req, res) => {
   const link = `http://localhost:5173/reset-password/${user._id}/${token}`;
 
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
